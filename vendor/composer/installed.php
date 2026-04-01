c78f58329131a73786c957e9383777901f44dd44
JoggingSplash <santisegura15@gmail.com>
2026-03-31 23:06:04 -0300

    uups...

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'cisco/multiversion-encoding',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '84ec5374e715efb72afd5a0f9641d36f7cdb1371',
+        'reference' => '476a8627f2e702571d0e7200a2d9d1abd57545bd',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'cisco/multiversion-encoding' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '84ec5374e715efb72afd5a0f9641d36f7cdb1371',
+            'reference' => '476a8627f2e702571d0e7200a2d9d1abd57545bd',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
